2106c39398b891700d0da72c076fe4a1bf3580f4
Bruno Maia <brunomaia2304@gmail.com>
2025-07-15 17:52:45 -0300

    Ajuste DAG

--- airflow/dags/solr_pipeline_dag.py
+++ airflow/dags/solr_pipeline_dag.py
@@ -1,8 +1,14 @@
+import sys
+from datetime import datetime, timedelta
+
+# Injeta a pasta de scripts no path do Python
+sys.path.insert(0, '/opt/airflow/scripts')
+
+from format_csv import format_csv
+from load_to_solr import load_to_solr
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime, timedelta
-from scripts.format_csv import format_csv
-from scripts.load_to_solr import load_to_solr
 
 default_args = {
     'owner': 'airflow',
@@ -16,7 +22,7 @@ with DAG(
     description='Formata CSV e carrega no Solr toda semana',
     schedule_interval='@weekly',
     default_args=default_args,
-    catchup=False
+    catchup=False,
 ) as dag:
 
     format_task = PythonOperator(
@@ -24,8 +30,8 @@ with DAG(
         python_callable=format_csv,
         op_kwargs={
             'input_path': '/opt/airflow/data/alunos.csv',
-            'output_path': '/opt/airflow/data/alunos_clean.csv'
-        }
+            'output_path': '/opt/airflow/data/alunos_clean.csv',
+        },
     )
 
     load_task = PythonOperator(
@@ -33,8 +39,8 @@ with DAG(
         python_callable=load_to_solr,
         op_kwargs={
             'solr_url': 'http://solr_instance:8983/solr/alunos_collection',
-            'csv_path': '/opt/airflow/data/alunos_clean.csv'
-        }
+            'csv_path': '/opt/airflow/data/alunos_clean.csv',
+        },
     )
 
     format_task >> load_task
